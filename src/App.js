a4ec18bfd4a479e7db6f59320f9c51a67a6f591a
ridhoauliama97 <ridhoauliama97@gmail.com>
2023-08-22 19:49:34 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useEffect } from 'react';
+import Clock from './Components/Clock';
+import AlarmContainer from './Components/AlarmContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAlarmsFromStorage } from './redux/actions/AlarmAction';
+
+function App() {
+  const activeAlarms = useSelector(state => state.alarmReducer.activeAlarm)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const myAlarms = localStorage.getItem('myAlarms');
+    if (myAlarms) {
+      dispatch(setAlarmsFromStorage(JSON.parse(myAlarms)))
+    }
+  }, [dispatch]);
+
+  return (
+    <div className="App" style={{ backgroundColor: activeAlarms ? activeAlarms.bgColor : "#000" }}>
+      <Clock />
+      <AlarmContainer />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/Components/Clock/index.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import moment from 'moment'
+import './Clock.css'
+
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTime } from '../../redux/actions/TimerAction';
+
+const Clock = () => {
+  const dispatch = useDispatch()
+  const time = useSelector(state => state.updateTimeReducer.time)
+  const activeAlarms = useSelector(state => state.alarmReducer.activeAlarm)
+
+  useEffect(() => {
+    const timerId = setInterval(() => dispatch(updateTime()), 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="clock-container">
+      <p className="time" style={{ color: activeAlarms ? "#232d31" : "#fff" }}>{moment(time).format('hh:mm:ss A')}</p>
+      <p className="date" style={{ color: activeAlarms ? "#232d31" : "#fff" }}>{moment(time).add(-1, "days").format('DD/MM/YYYY')}</p>
+    </div>
+  )
+}
+
+export default Clock
